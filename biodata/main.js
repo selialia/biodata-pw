b4196a1f5058bf5b9b7d12e71f1ef85ce77ea3a1
selialiaputri <142282146+selialia@users.noreply.github.com>
2023-12-12 09:50:33 +0700

    Add files via upload

---
+++ biodata/main.js
@@ -0,0 +1,7 @@
+let nama = "Nama: Seli alia putri";
+let kelas = "Kelas: XI";
+let jurusan = "Jurusan: PPLG";
+let umur = "Umur: 16";
+let alamat = "Alamat: Kp.cidahu";
+
+document.write(nama + "<br>", kelas + "<br>", jurusan + "<br>", umur + "<br>", alamat + "<br>");
